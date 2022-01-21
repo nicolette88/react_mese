82fbe9bbbdb9864a20f528cea5b183ac6a805b84
Niki <nicolette88@gmail.com>
2022-01-21 15:07:40 +0100

    react alap

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Add to your list</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const TodoItem = props => {
+  return (
+    <li className="todo-item">{props.title}
+      <span className="remove-button"
+        onClick={() => { props.removeHandler(props.id); }}
+      >
+      </span>
+    </li>
+  );
+};
+export default TodoItem;
